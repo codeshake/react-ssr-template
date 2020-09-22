fe3f64c1c8a8e055f5d1bc5e0e47a5a8dc980073
Sergey Lutov <doublemakers@gmail.com>
2020-09-22 22:27:51 +0300

    Hydrate method fix

--- src/Client.tsx
+++ src/Client.tsx
@@ -1,16 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {
-    BrowserRouter,
-} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { App } from 'App'
 
 const entryBlock = document.getElementById('root')
+const renderFunction: ReactDOM.Renderer = entryBlock.hasChildNodes() ? ReactDOM.hydrate : ReactDOM.render
 
-if (entryBlock.hasChildNodes() === true) {
-    ReactDOM.render(<BrowserRouter>
-        <App />
-    </BrowserRouter>, entryBlock)
-} else {
-    ReactDOM.hydrate(<App />, entryBlock)
-}
+renderFunction(<BrowserRouter>
+    <App />
+</BrowserRouter>, entryBlock)
